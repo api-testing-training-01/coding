427d63028130b693cc7de30bf3d5b2bd2d7e8e6b
JorgeOrellana <orellana.mendoza.je@gmail.com>
2020-03-10 23:18:22 -0400

    making the code work

--- src/main/java/org/fundacionjala/coding/SpinWords.java
+++ src/main/java/org/fundacionjala/coding/SpinWords.java
@@ -3,6 +3,19 @@ package org.fundacionjala.coding;
 public class SpinWords {
 
     public static String convertToSpin(String text) {
-        return null;
+        String[] splitText = text.split(" ");
+        String spinResult = "";
+        for (int i=0 ; i< splitText.length ; i++) {
+            String word = splitText[i];
+            if (!word.isEmpty() && word.length() > 4) {
+                spinResult += new StringBuilder(word).reverse().toString();
+                spinResult += " ";
+            } else {
+                spinResult += word;
+                spinResult += " ";
+            }
+        }
+
+        return spinResult.substring(0, spinResult.length() - 1);
     }
 }
--- src/test/java/org/fundacionjala/coding/SpinWordsTest.java
+++ src/test/java/org/fundacionjala/coding/SpinWordsTest.java
@@ -7,9 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class SpinWordsTest {
 
     @Test
-    public void spinWords() {
+    public void spinWordsOne() {
         String text = "Hey fellow warriors";
         String expectedResult = "Hey wollef sroirraw";
+        String actualText = SpinWords.convertToSpin(text);
+        assertEquals(expectedResult, actualText);
+    }
+
+    @Test
+    public void spinWordsTwo() {
+        String text = "This is a test";
+        String expectedResult = "This is a test";
+        String actualText = SpinWords.convertToSpin(text);
+        assertEquals(expectedResult, actualText);
+    }
+
+    @Test
+    public void spinWordsThree() {
+        String text = "This is another test";
+        String expectedResult = "This is rehtona test";
         String actualText = SpinWords.convertToSpin(text);
         assertEquals(expectedResult, actualText);
     }
