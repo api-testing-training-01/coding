c069b460fff3019312868375d34255d8f3c1a49a
JorgeOrellana <orellana.mendoza.je@gmail.com>
2020-03-13 00:24:02 -0400

    wrinting test

--- src/main/java/org/fundacionjala/coding/jorge/HighestAndLowest.java
+++ src/main/java/org/fundacionjala/coding/jorge/HighestAndLowest.java
@@ -1,11 +1,22 @@
 package org.fundacionjala.coding.jorge;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public final class HighestAndLowest {
 
     private HighestAndLowest() {
     }
 
     public static String findHighestAndLowest(final String text) {
-        return null;
+        String[] numbers = text.split(" ");
+        Arrays.sort(numbers, new Comparator<String>() {
+            @Override
+            public int compare(final String s, final String t1) {
+                return Integer.valueOf(s).compareTo(Integer.valueOf(t1));
+            }
+        });
+        return numbers[numbers.length - 1] + " " + numbers[0];
     }
+
 }
