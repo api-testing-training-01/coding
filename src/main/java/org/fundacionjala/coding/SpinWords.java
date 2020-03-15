1acfb9d15d59c153bf5c1cd49b76b52665f64eab
Alejandra Neolopan <alejandra.neolopan@gmail.com>
2020-03-14 22:58:37 -0400

    Solving style observations

--- src/main/java/org/fundacionjala/coding/SpinWords.java
+++ src/main/java/org/fundacionjala/coding/SpinWords.java
@@ -5,22 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 /**
- * Codewars exercise'.
+ * Codewars exercise 1.
  */
 public class SpinWords {
     /**
-     * spinWords return a sentence with phrases in reverse order when word length >5
-     *
+     * spinWords return a sentence with phrases in reverse order when word length > 5.
+     * @param  sentence  the string that contains sentences
      * @return String
      */
-    public String spinWords(String sentence) {
+    public String spinWords(final String sentence) {
         //Streaming string to filter by length to apply reverse
-       List<String> sentenceArray =Arrays.asList( sentence.split(" "));
+        final int LIMIT_LENGTH = 5;
+       List<String> sentenceArray = Arrays.asList(sentence.split(" "));
        List<String> resultArray = new ArrayList<>();
        sentenceArray.stream()
                 .forEach(word -> {
-                    if(word.length() > 5)
+                    if (word.length() > LIMIT_LENGTH){
                         word = reverse(word);
+                    }
                     resultArray.add(word);
                 });
 
@@ -28,18 +30,22 @@ public class SpinWords {
         return result;
     }
 
-    private String reverse(String word5) {
+    private String reverse(final String word5) {
         StringBuilder res = new StringBuilder(word5);
 
         int i = 0, j = word5.length() - 1;
         while (i < j) {
-            while ((i < j) && (!Character.isLetter(word5.charAt(i)))) i ++;
-            while ((i < j) && (!Character.isLetter(word5.charAt(j)))) j --;
+            while ((i < j) && (!Character.isLetter(word5.charAt(i)))){
+                i++;
+            }
+            while ((i < j) && (!Character.isLetter(word5.charAt(j)))){
+                j--;
+            }
             char tmp = word5.charAt(i);
-            res.setCharAt(i, word5.charAt(j)) ;
+            res.setCharAt(i, word5.charAt(j));
             res.setCharAt(j, tmp);
-            i ++;
-            j --;
+            i++;
+            j--;
         }
         return String.valueOf(res);
     }
